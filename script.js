a6ece1414edcec10c2a3e836a741f685e6188cad
Anil Kumar Yadav <anilyadav10566@gmail.com>
2025-05-01 12:29:32 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let input=document.getElementById("inputBox");
+let button =document.querySelectorAll
+("button");
+
+let string= " ";
+let arr=Array.from(button);
+arr.forEach(button=>{
+    button.addEventListener("click",(a)=>{
+        if(a.target.innerHTML=="="){
+            string = eval(string);
+            input.value =string;
+        }
+        else if(a.target.innerHTML=="OFF"){
+            string="";
+            input.value=string;
+        }
+        else if(a.target.innerHTML=="AC"){
+            string="";
+            input.value=string;
+        }
+        else if(a.target.innerHTML=="DEL"){
+            string = string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else{
+            string += a.target.innerHTML;
+            input.value=string;
+        }
+    })
+})
